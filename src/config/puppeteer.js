0e1221c2db3098a53f22fe36a601d212aed00a4e
Sutan Nasution <sutan.gnst@gmail.com>
2020-02-16 20:46:08 +0700

    update config

--- src/config/puppeteer.js
+++ src/config/puppeteer.js
@@ -4,6 +4,6 @@ module.exports = () => chrome.executablePath
     .then(executablePath => chrome.puppeteer.launch({
         executablePath,
         args: chrome.args,
-        // headless: true
-        headless: chrome.headless
+        headless: true
+        // headless: chrome.headless
     }))
